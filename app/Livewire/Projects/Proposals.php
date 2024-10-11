8b15c75a41281fb794fa7152627b08da99ec4eb7
Guilherme <guilhermebflores@gmail.com>
2024-10-11 17:43:46 -0300

    create some controllers and components

---
+++ app/Livewire/Projects/Proposals.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire\Projects;
+
+use Livewire\Component;
+
+class Proposals extends Component
+{
+    
+    public function render()
+    {
+        return view('livewire.projects.proposals');
+    }
+}
